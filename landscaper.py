10989a8b85a5def9fac9165b00ef4544d15b1398
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-03-05 23:14:36 -0500

    all functionality complete, fine tuning tbc

--- landscaper.py
+++ landscaper.py
@@ -1,6 +1,8 @@
+import sys
+
 player = {
     "money": 0,
-    "tool": 0
+    "tools": 0
 }
 
 tools = [
@@ -12,20 +14,57 @@ tools = [
 ]
 
 def getInput():
-    result = input("You may [m]ow, [u]pgrade, or [r]eset")
+    result = input("You may [m]ow, [u]pgrade, [r]eset, or [c]lose. Choose one.")
 
     if(result == "m"):
         earnMoney()
         return 1
     
     if(result == "u"):
-        upgradeTool()
-        return 2
+        upgradeTools()
+        return 1
     
     if(result == "r"):
         reset()
-        return 3
+        return 1
+    
+    if(result == "c"):
+        close()
+        return 1
     
     print("choose a valid option")
     getInput()
+
+def earnMoney():
+    print(f"You mowed a lawn and earned ${tools[player["tools"]]["earns"]}!")
+    player["money"] += tools[player["tools"]]["earns"]
+    print (player["money"])
+    win()
+    getInput()
+
+def upgradeTools():
+    print(f"You have purchased a new tool!")
+    player["tools"] += 1
+    print(tools[player["tools"]])
+    win()
+    getInput()
+
+def reset():
+    print("You've restarted the game. Time to get mowing!")
+    player["money"] == 0
+    tools[player["tools"]] == 0
+    getInput()
+
+def close():
+    print("Goodbye!")
+    sys.exit()
+
+def win():
+  if player["money"] == 1000 and tools[player["tools"]]["name"] == "students":
+    return "You have won! Congratulations!"
+  else:
+    print("You haven't won yet! Keep on mowing!")
+    getInput()
+
+getInput()
 
