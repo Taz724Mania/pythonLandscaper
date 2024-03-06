93aabbc21fc7ee7e1385d330b8e0ddce246ee25e
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-03-05 22:40:40 -0500

    Input form created

--- landscaper.py
+++ landscaper.py
@@ -11,3 +11,21 @@ tools = [
     {"name": "students", "price": 500, "earns": 250},
 ]
 
+def getInput():
+    result = input("You may [m]ow, [u]pgrade, or [r]eset")
+
+    if(result == "m"):
+        earnMoney()
+        return 1
+    
+    if(result == "u"):
+        upgradeTool()
+        return 2
+    
+    if(result == "r"):
+        reset()
+        return 3
+    
+    print("choose a valid option")
+    getInput()
+
