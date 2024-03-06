bb8b3def69f07664312d9e7dbe50c323df3c8bef
Taz Campbell <shelbyacampbell2724@gmail.com>
2024-03-05 22:36:28 -0500

    info added

---
+++ landscaper.py
@@ -0,0 +1,13 @@
+player = {
+    "money": 0,
+    "tool": 0
+}
+
+tools = [
+    {"name": "teeth", "price": 0, "earns": 1},
+    {"name": "scissors", "price": 5, "earns": 5},
+    {"name": "push-mower", "price": 25, "earns": 50},
+    {"name": "electric-mower", "price": 250, "earns": 100},
+    {"name": "students", "price": 500, "earns": 250},
+]
+
